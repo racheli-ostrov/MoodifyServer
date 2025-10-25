276579ebde5be3bf0d82f05c407eb7f82d6578eb
racheli-ostrov <Hand4d1@mbjcomp.org.il>
2025-10-26 01:01:33 +0300

    add cookie-parser dependency

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const paypalRoutes = require('./routes/payPal');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true
 }));
 
