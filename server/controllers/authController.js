1dcc8589ad1ae32758a5a1b23bca1b478b393d76
racheli-ostrov <Hand4d1@mbjcomp.org.il>
2025-11-05 00:38:36 +0200

    vvv

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -234,7 +234,8 @@
 // }
 import { OAuth2Client } from "google-auth-library";
 import jwt from "jsonwebtoken";
-import User from "../../models/User.js"; // ← לוודא שקיים הקובץ הזה
+import User from "../models/User.js";
+
 
 // יצירת לקוח Google
 const client = new OAuth2Client(process.env.VITE_GOOGLE_CLIENT_ID);
